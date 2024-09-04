d761cfff1c80a632aa74e5d27737d592005af69d
Selawen <37039183+Selawen@users.noreply.github.com>
2024-09-04 14:31:22 +0200

    Added main pages, header, and footer

---
+++ index.php
@@ -0,0 +1,36 @@
+//<?php include"switch.php";?>
+
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>Inger Smit Home</TITLE>
+        <link href="stylesheet.css" rel="stylesheet" type="text/css" media="screen">
+        <meta name="robots" content="all"/>
+        <meta name="googlebot-news" content="noindex">
+    </HEAD>
+	<BODY>
+		<header><?php include "header.inc";?></header>
+        <nav><?php include "menu.inc";?></nav>
+        <div id="content">
+            <H1>Home</H1>
+            <div id="home">
+            <section id="welkom">
+                <H2>Hi I'm Inger!</H2>
+                <img src="Images/me.jpg">
+                <div class="card">
+                    <P>
+                        I've always been the kind of person that would bounce from hobby to hobby. Taking in and learning as much as I could before moving on to the next. My everchanging passion projects have taught me to be flexible and quickly grasp the essence of a particular subject. This has given me a very broad foundation to build my skills on. My focus is more on the scripting side of game development. But I also greatly enjoy and can actively participate in (almost) every part of the creation process.
+                    </P>
+                    <P>
+                        I enjoy working in teams where every person can voice their concerns and opinions on the problem at hand. I will voice my thoughts and want others to do the same. A product realised through the effort and input of the full team will always be greater than any individual project, however talented the individual making it may be.
+                    </P>
+                    <P>
+                        If you want to work together on a great project or talk about something interesting don’t hesitate to shoot me a message!
+                    </P>
+                </div>
+            </section>
+            </div>
+        </div>
+		<footer><?php include "footer.inc"?></footer>
+	</BODY>
+</HTML>
---
+++ portfolio.php
@@ -0,0 +1,19 @@
+<?php include"switch.php";?>
+
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>Inger Smit Home</TITLE>
+        <link href="stylesheet.css" rel="stylesheet" type="text/css" media="screen">
+        <meta name="robots" content="all"/>
+	</HEAD>
+	<BODY>
+		<header><?php include "header.inc";?></header>
+        <nav><?php include "menu.inc";?></nav>
+        <div id="content">
+            <H1>Portfolio</H1>
+            <H3>Insert work here</H3>
+        </div>
+		<footer><?php include "footer.inc"?></footer>
+    </BODY>
+</HTML>
